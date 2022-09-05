3608a4522dead8e84f2d88336040fbd87ca4f38d
Kyoungwon kang <kkwondev@gmail.com>
2022-09-05 18:35:38 +0900

    #1.0 ~ #1.4

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -6,7 +8,22 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(3000, () => console.log("server open http://localhost:3000"));
+const handleListen = () =>
+  console.log(`Listening on http://localhost:3000 ws://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser!");
+  socket.on("close", () => console.log("close brower!"));
+  socket.on("message", (message) => {
+    console.log(message.toString("utf-8"));
+  });
+  socket.send("hello!!!");
+});
+
+server.listen(3000, handleListen);
